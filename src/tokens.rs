051be181937f645888e3c055f5f05062a9b475d7
Koyin2005 <koyinsolaoshodi@hotmail.com>
2026-04-22 23:04:49 +0100

    Added new tokens

---
+++ src/tokens.rs
@@ -0,0 +1,22 @@
+pub struct Token{
+    kind : TokenKind
+}
+pub enum TokenKind {
+    Ident(String),
+    Fun,
+    Arrow,
+    LeftBrace,
+    RightBrace,
+    Let,
+    Mut,
+    Equal,
+    Imm,
+    Some,
+    In,
+    None,
+    Panic,
+    Coma,
+    Print,
+    Borrow,
+    For,
+}
