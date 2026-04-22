1287b6292eeaab33e23b534ee866cbe080fb10c9
Koyin2005 <koyinsolaoshodi@hotmail.com>
2026-04-22 23:00:56 +0100

    Added tokens and parse

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+use std::env;
+
+
 fn main() {
-    println!("Hello, world!");
+    let mut args = env::args().skip(1).collect::<Vec<_>>();
+    let path = if args.len() == 1 && let Some(name) = args.pop() {
+        name
+    }
+    else {
+        eprintln!("Invalid format");
+        eprintln!("Expected 'program_path'");
+        return;
+    };
+    let src = match std::fs::read_to_string(&path){
+        Ok(src) => src,
+        Err(e) => {
+            match e.kind(){
+                std::io::ErrorKind::NotFound => {
+                    eprintln!("File at '{}' not found",path);
+                    return;
+                },
+                _ => {
+                    eprintln!("Unknown error '{:?}'",e);
+                    return;
+                }
+            }
+        }
+    };
+    println!("{}",src);
 }
