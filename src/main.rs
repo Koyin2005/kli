2e908d00562862f4df36905b0f38706d6d53a7c4
Koyin_2005 <koyinsolaoshodi@hotmail.com>
2026-07-01 18:55:23 +0100

    Not making mir for builtin functions

--- src/main.rs
+++ src/main.rs
@@ -310,6 +310,9 @@ fn main() {
     }
     let mut mir_context = mir::Context::new(true);
     for (&id, function) in program.functions.iter() {
+        if context.as_ref().builtins().builtin_for(id).is_some(){
+            continue;
+        }
         mir::build::Builder::build_from_function(context.as_ref(), &mut mir_context, id, function);
     }
     if config.flags.contains(&Feature::OutputMir) {
