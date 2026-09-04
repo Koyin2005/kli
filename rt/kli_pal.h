293b0fd641f731bad3dfab575e32e330b6364b34
Koyin_2005 <koyinsolaoshodi@hotmail.com>
2026-09-04 22:24:07 +0100

    Removed rt

--- rt/kli_pal.h
+++
@@ -1,4 +0,0 @@
-#include <stdint.h>
-void kli_print_string(uint8_t *ptr, size_t len);
-void kli_eprint_string(uint8_t *ptr, size_t len);
-size_t kli_read_string(uint8_t *ptr, size_t len);
--- rt/kli_rt.c
+++
@@ -1,5 +0,0 @@
-#include <stdio.h>
-#include <inttypes.h>
-#include <stdint.h>
-#include <stdlib.h>
-#include "kli_pal.h"
