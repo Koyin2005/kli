92bb6d88c0a52997c42c4cce6690c1b4939e5756
Koyin_2005 <koyinsolaoshodi@hotmail.com>
2026-09-19 01:44:44 +0100

    Added more to ir

--- src/ir/mod.rs
+++ src/ir/mod.rs
@@ -1,64 +1,80 @@
-use crate::{Symbol, def_ids::DefId, define_id, index_vec::IndexVec, typed_ast::FieldId};
+use crate::{
+    Symbol, def_ids::DefId, define_id, index_vec::IndexVec, typed_ast::FieldId, types::CaseId,
+};
 
 pub enum Constant {
     Int(i64),
     Bool(bool),
-    Function(DefId)
+    Function(DefId),
 }
-pub struct Expr{
-    pub kind : ExprKind
+pub struct Expr {
+    pub kind: ExprKind,
+}
+pub enum AggregateKind {
+    Tuple,
 }
 pub enum ExprKind {
     Constant(Constant),
     Load(Place),
     Tuple(Vec<Expr>),
-    Discriminant(Place)
+    Discriminant(Place),
+    Aggregate(AggregateKind, IndexVec<FieldId, Expr>),
 }
 define_id!(Local);
 pub enum Place {
     Local(Local),
-    Field(Box<Place>,FieldId)
+    Field(Box<Place>, FieldId),
+    Deref(Box<Place>),
+    Downcast(Box<Place>, CaseId),
 }
 
-pub struct SwitchArm{
-    pub value : i32,
-    pub body : Vec<Stmt>
+pub struct SwitchArm {
+    pub value: i32,
+    pub body: DecisionTree,
 }
-pub struct Switch{
-    pub value : Expr,
-    pub arms : Vec<SwitchArm>,
-    pub otherwise : Box<Stmt>
+pub struct Switch {
+    pub value: Expr,
+    pub arms: Vec<SwitchArm>,
+    pub otherwise: Box<DecisionTree>,
 }
-pub struct Call{
-    pub return_place : Place,
-    pub callee : Expr,
-    pub args : Vec<Expr>
+pub struct Call {
+    pub return_place: Place,
+    pub callee: Expr,
+    pub args: Vec<Expr>,
+}
+define_id!(ArmId);
+pub enum DecisionTree {
+    Body(ArmId),
+    Switch(Switch),
 }
 define_id!(LoopLabel);
-pub struct Loop{
-    pub label : LoopLabel,
-    pub stmts : Vec<Stmt>
+pub struct Loop {
+    pub label: LoopLabel,
+    pub stmts: Vec<Stmt>,
+}
+pub struct Match{
+    pub tree : DecisionTree,
+    pub arms : IndexVec<ArmId,Vec<Stmt>>
 }
 pub enum Stmt {
     Block(Vec<Stmt>),
-    Loop(LoopLabel,Vec<Stmt>),
+    Loop(LoopLabel, Vec<Stmt>),
     Break(LoopLabel),
-    If(Expr,Vec<Stmt>,Vec<Stmt>),
-    Switch(Switch),
+    If(Expr, Vec<Stmt>, Vec<Stmt>),
+    Match(Match),
     Call(Call),
-    Assign(Place,Expr),
-    Return(Expr)
+    Assign(Place, Expr),
+    Return(Expr),
 }
 pub type Type = ();
-pub struct LocalInfo{
-    pub name : Option<Symbol>,
-    pub is_mutable : bool,
-    pub ty : Type
+pub struct LocalInfo {
+    pub name: Option<Symbol>,
+    pub is_mutable: bool,
+    pub ty: Type,
 }
-pub struct Body{
-    pub param_count : u32,
-    pub return_ty : Type,
-    pub locals : IndexVec<Local,LocalInfo>,
-    pub body : Vec<Stmt>   
-    
+pub struct Body {
+    pub param_count: u32,
+    pub return_ty: Type,
+    pub locals: IndexVec<Local, LocalInfo>,
+    pub body: Vec<Stmt>,
 }
--- src/vm/instructions.rs
+++ src/vm/instructions.rs
@@ -2,16 +2,12 @@ pub struct Reg(u16);
 pub struct FunctionId(u32);
 pub struct JumpOffset(i32);
 pub enum Instr {
-    Add{
-        dst : Reg,
-        src1 : Reg,
-        src2 : Reg,
-    },
+    Add { dst: Reg, src1: Reg, src2: Reg },
     Push(Reg),
     Pop(Reg),
     Call(FunctionId),
     CallIndirect(Reg),
-    JumpIf(Reg,JumpOffset),
+    JumpIf(Reg, JumpOffset),
     Jump(JumpOffset),
-    Return
+    Return,
 }
